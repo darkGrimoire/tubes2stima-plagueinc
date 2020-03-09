246afbc3217c86209a28b862d580916ed309d491
regnents <rafaelseanputra@gmail.com>
2020-03-09 22:04:05 +0700

    Ini versi test case menggunakan output textbox, bukan graph

--- Plague.inc STEAMA/Form1.Designer.cs
+++ Plague.inc STEAMA/Form1.Designer.cs
@@ -31,6 +31,11 @@
             this.panelSideMenu = new System.Windows.Forms.Panel();
             this.panelLowerThird = new System.Windows.Forms.Panel();
             this.panelMainMenu = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panelMainMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelSideMenu
@@ -58,12 +63,47 @@
             // panelMainMenu
             // 
             this.panelMainMenu.BackColor = System.Drawing.Color.Black;
+            this.panelMainMenu.Controls.Add(this.textBox3);
+            this.panelMainMenu.Controls.Add(this.button1);
+            this.panelMainMenu.Controls.Add(this.textBox2);
+            this.panelMainMenu.Controls.Add(this.textBox1);
             this.panelMainMenu.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelMainMenu.Location = new System.Drawing.Point(200, 0);
             this.panelMainMenu.Name = "panelMainMenu";
             this.panelMainMenu.Size = new System.Drawing.Size(836, 522);
             this.panelMainMenu.TabIndex = 2;
             this.panelMainMenu.Paint += new System.Windows.Forms.PaintEventHandler(this.panelMainMenu_Paint);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(191, 193);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(265, 367);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(191, 130);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(300, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(191, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(228, 22);
+            this.textBox1.TabIndex = 0;
             // 
             // Form1
             // 
@@ -77,6 +117,8 @@
             this.Name = "Form1";
             this.Text = "Plague .inc - STEAMA ver.";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelMainMenu.ResumeLayout(false);
+            this.panelMainMenu.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -86,6 +128,10 @@
         private System.Windows.Forms.Panel panelSideMenu;
         private System.Windows.Forms.Panel panelLowerThird;
         private System.Windows.Forms.Panel panelMainMenu;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- Plague.inc STEAMA/Form1.cs
+++ Plague.inc STEAMA/Form1.cs
@@ -11,6 +11,7 @@ using System.Collections;
 
 namespace Plague.inc_STEAMA
 {
+    
     public partial class Form1 : Form
     {
         public Form1()
@@ -37,18 +38,38 @@ namespace Plague.inc_STEAMA
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string temp2 = textBox2.Text;
+            int hari = Convert.ToInt32(temp2);
+            PlagueInc testing = new PlagueInc();
+            string yes = testing.mulaiPenyebaran(testing.getCity(), hari);
+            textBox3.Text = yes;
+        }
+
     }
 
     public class City 
     {
         private Dictionary<City, double> neighbors;
+        private string nama;
         private int penduduk, waktuInfeksiAwal;
         private bool statusInfeksi;
+        
+        public City(int penduduk, string name)
+        {
+            this.penduduk = penduduk;
+            this.waktuInfeksiAwal = 0;
+            this.statusInfeksi = false;
+            this.nama = name;
+            this.neighbors = new Dictionary<City, double>();
+        }
         public int getPenduduk()
         {
             return penduduk;
         }
-        public int getT()
+        public int getWaktuInfeksiAwal()
         {
             return waktuInfeksiAwal;
         }
@@ -60,39 +81,71 @@ namespace Plague.inc_STEAMA
         {
             return statusInfeksi;
         }
+        public string getNama()
+        {
+            return nama;
+        }
 
+        public void setPenduduk(int penduduk)
+        {
+            this.penduduk = penduduk;
+        }
         public void setWaktuInfeksiAwal(int waktuInfeksiAwal)
         {
             this.waktuInfeksiAwal = waktuInfeksiAwal;
         }
+        public void setNeighbors(City B, double test)
+        {
+            this.neighbors.Add(B, test);
+        }
+       
         public void setStatusInfeksi()
         {
-            statusInfeksi = !(statusInfeksi);
+            this.statusInfeksi = !(this.statusInfeksi);
         }
     }
-
-    /*public class ListCity
-    {
-        private List<City> daftarKota;
-        private int jumlahKota;
-        public int getJumlahKota()
-        {
-            return jumlahKota;
-        }
-        public City getCity(int temp)
-        {
-            return daftarKota[temp];
-        }
-
-    }*/
 
     public class PlagueInc 
     {
         private List<City> daftarKota;
         private int jumlahKota;
+        
+
+        public PlagueInc ()
+        {
+            this.jumlahKota = 4;
+            this.daftarKota = new List<City>();
+            City a = new City(1000, "a");
+            City b = new City(5000, "b");
+            City c = new City(1000, "c");
+            City d = new City(1000, "d");
+            a.setNeighbors(b, 0.02);
+            a.setNeighbors(c, 0.005); 
+            a.setNeighbors(d, 0);
+            b.setNeighbors(a, 0.005);
+            b.setNeighbors(c, 0);
+            b.setNeighbors(d, 0.005);
+            c.setNeighbors(a, 0.1);
+            c.setNeighbors(b, 0.1);
+            c.setNeighbors(d, 0);
+            d.setNeighbors(a, 0.05);
+            d.setNeighbors(b, 0);
+            d.setNeighbors(c, 0.1);
+            daftarKota.Add(a);
+            daftarKota.Add(b);
+            daftarKota.Add(c);
+            daftarKota.Add(d);
+        }
+        public City getCity()
+        {
+            return this.daftarKota[0];
+        }
+        
+        
         public double fungsiLogistik(City kota, int hari)
         {
-            return (kota.getPenduduk() / (1 + (kota.getPenduduk() - 1)*Math.Exp(-0.25*hari-kota.getT())));
+            /*return ( kota.getPenduduk() / (1 + (kota.getPenduduk() - 1)*Math.Exp(-0.25*hari-kota.getPenduduk())) );*/
+            return hari * kota.getPenduduk() / 20;
         }
 
         public double fungsiPenyebaran(City A, City B, int hari)
@@ -102,19 +155,36 @@ namespace Plague.inc_STEAMA
 
         public int getWaktuPenyebaran(City A, City B)
         {
-            return Convert.ToInt32(Math.Ceiling(Math.Log((A.getPenduduk() * A.getNeighbors(B) - 1) / (A.getPenduduk() - 1)) / (-0.25)));
+            /* double temp =Math.Log((A.getPenduduk() * A.getNeighbors(B) - 1) / (A.getPenduduk() - 1)) / (-0.25);
+               if (temp - Math.Floor(temp) > 0)
+               {
+                    return Convert.ToInt32(Math.Ceiling(temp));
+               }
+               else
+               {
+                    return Convert.ToInt32(temp + 1);
+               }*/
+            double temp = 20 / ( A.getPenduduk() * A.getNeighbors(B) )  ;
+            if (temp - Math.Floor(temp) > 0)
+            {
+                return Convert.ToInt32(Math.Ceiling(temp));
+            }
+            else
+            {
+                return Convert.ToInt32(temp + 1);
+            }
         }
 
-        public void mulaiPenyebaran(City kotaAwal, int batasHari)
+        public string mulaiPenyebaran(City kotaAwal, int batasHari)
         {
-            List<City> tempAntrian = new List<City>();
-            tempAntrian.Add(kotaAwal);
+            List<City> tempAntrian = new List<City>();  /* Queue dari City yang akan dicek */
+            tempAntrian.Add(kotaAwal);                  /* Menambah City tempat infeksi pertama kali muncul ke Queue */
             kotaAwal.setStatusInfeksi();
-
+            City nowKota = kotaAwal;
             while (tempAntrian.Count != 0)
             {
-                City prevKota = kotaAwal; /* Menampung City yang sebelumnya dicek */
-                City nowKota = tempAntrian[0]; /*Menampung City yang akan dicek */
+                City prevKota = nowKota;       /* Menampung City yang sebelumnya dicek */
+                nowKota = tempAntrian[0];  /*Menampung City yang akan dicek */
                 tempAntrian.RemoveAt(0);
                 for (int i = 0; i < jumlahKota; i++)
                 {
@@ -123,20 +193,31 @@ namespace Plague.inc_STEAMA
                         (nowKota.getNeighbors(kotaSelanjutnya) != 0) && 
                         (kotaSelanjutnya != prevKota) )
                     {
-                        double probabilitasPenyebaran = fungsiPenyebaran(nowKota, kotaSelanjutnya, batasHari);
+                        double probabilitasPenyebaran = fungsiPenyebaran(nowKota, kotaSelanjutnya, batasHari - nowKota.getWaktuInfeksiAwal());
                         if (probabilitasPenyebaran > 1)
                         {
                             int waktuPenyebaran = getWaktuPenyebaran(nowKota, kotaSelanjutnya);
                             if (!kotaSelanjutnya.getStatusInfeksi())
                             {
                                 kotaSelanjutnya.setStatusInfeksi();
-                                kotaSelanjutnya.setWaktuInfeksiAwal(waktuPenyebaran);
+                                kotaSelanjutnya.setWaktuInfeksiAwal(waktuPenyebaran + nowKota.getWaktuInfeksiAwal());
                                 tempAntrian.Add(kotaSelanjutnya);
                             }
                         }
                     }
                 }
             }
+            string tempText = "";
+            for (int i = 0; i< jumlahKota; i++)
+            {
+                if (daftarKota[i].getStatusInfeksi())
+                {
+                    tempText = tempText + daftarKota[i].getNama() + " ";
+                }
+            }
+
+            return tempText;
+
         }
 
         
